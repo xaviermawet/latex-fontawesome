beca1257806960b7135f5c868e0d8d828613f173
Xavier Mawet <xavier@nakim.be>
2022-03-08 02:18:20 +0100

    Added fontawesome local module

--- src/app.js
+++ src/app.js
@@ -1,24 +1,11 @@
-import { request, gql } from "graphql-request";
+import { getIcons } from "./lib/fontawesome.js";
 
 async function main() {
-  const endpoint = "https://api.fontawesome.com";
-  const query = gql`
-    {
-      release(version: "6.0.0") {
-        version
-        icons {
-          id
-          unicode
-          membership {
-            free
-          }
-        }
-      }
-    }
-  `;
+  const version = "6.0.0";
+  const includePro = false;
 
-  const data = await request(endpoint, query);
-  console.log(JSON.stringify(data, undefined, 2));
+  const icons = await getIcons(version, includePro);
+  console.log(JSON.stringify(icons, undefined, 2));
 }
 
 main().catch((error) => console.error(error));
---
+++ src/lib/fontawesome.js
@@ -0,0 +1,32 @@
+import { request, gql } from "graphql-request";
+
+const endpoint = "https://api.fontawesome.com";
+
+function getRelease(version, includePro) {
+  const query = gql`
+    {
+      release(version: "${version}") {
+        version
+        icons {
+          id
+          unicode
+          membership {
+            free
+            ${includePro ? "pro" : ""}
+          }
+        }
+      }
+    }
+  `;
+
+  return request(endpoint, query);
+}
+
+async function getIcons(version, includePro) {
+  const data = await getRelease(version);
+  return includePro
+    ? data.release.icons
+    : data.release.icons.filter((icon) => icon.membership.free.length > 0);
+}
+
+export { getIcons };
