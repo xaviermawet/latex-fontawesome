7417fc9d966f37445e7dedada685db8afb19378a
Xavier Mawet <xavier@nakim.be>
2022-03-28 01:15:04 +0200

    Added commander - command line arguments parser

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
+import { program } from 'commander';
 import getIcons from './lib/fontawesome.js';
 import buildSty from './lib/sty.js';
 
 async function main() {
-  const version = '6.1.0';
-  const includePro = false;
-  const outputFile = 'fontawesome-latest.sty';
+  program
+    .name('latex-fontawesome')
+    .description('Bindings for Font Awesome icons to be used in XeLaTeX.')
+    .version('0.0.1');
 
-  const icons = await getIcons(version, includePro);
-  await buildSty(outputFile, icons);
+  program
+    .option('-r, --release <release>', 'Font Awesome release version', '6.0.0')
+    .option('-p, --pro', 'Include pro icons', false)
+    .option(
+      '-f, --filename <filename>',
+      'Output filename',
+      'fontawesome-latest.sty'
+    )
+    .parse();
+
+  const options = program.opts();
+
+  const icons = await getIcons(options.release, options.pro);
+  await buildSty(options.filename, icons);
 }
 
 main().catch((error) => console.error(error));
